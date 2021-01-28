aff7a02aebae38bab989b3c507e421051b34457c
Shami Tomita <shamitomita@gmail.com>
2021-01-28 15:40:23 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @artist = artist
+    @genre = genre 
+    save
+  end 
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def artist_name
+    self.artist.name  
+  end 
+  
+end 
